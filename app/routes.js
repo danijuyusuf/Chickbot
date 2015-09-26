1537509b90bfb4c6db148819f2850a2229dba39d
danijuyusuf <danijuyusuf@gmail.com>
2015-09-26 15:31:58 +0100

    Created instance of users

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ var User = require('./models/user');
         // authentication routes
 
         // route to handle creating goes here (app.post)
-
+        
         app.post('/user/new', function(req, res) {
             User.create({
                 firstname: req.body.firstname,
@@ -23,11 +23,20 @@ var User = require('./models/user');
                 modifiedOn : Date.now(),
                 lastLogin : Date.now()
                 }, function( err, user ){
-                if(!err){
-                console.log("User created and saved for: " + user.firstname + " " + user.lastname);
-                }
+                    if(err) {
+                        console.log(err);
+                        if(err.code === 11000) {
+                            res.redirect('/user/new?exists=true');
+                        } else {
+                            res.redirect('/?error=true');
+                        }
+                    } else {
+                        //Success
+                        console.log("User created and saved for: " + user.firstname + " " + user.lastname);
+                        res.redirect('/dashboard/admin')
+                    }
             });
-            res.redirect('/dashboard/admin')
+            
             
             });
         // route to handle delete goes here (app.delete)
